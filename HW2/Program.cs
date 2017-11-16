9380c14042a3086f3acaf56e62272c3eb58d85c4
jacelightwood7 <32211639+jacelightwood7@users.noreply.github.com>
2017-11-15 18:49:08 -0600

    Add files via upload

---
+++ HW2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nlastNum = 1;
+            int nnextToLastNum = 1;
+            int fibNum;
+            int currentNum=0;
+            //promp user
+
+            Console.WriteLine("enter whitch Fibonacci number you would like to see");
+            fibNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < fibNum; i++)
+            {
+                currentNum = nlastNum + nnextToLastNum;
+                nnextToLastNum = nlastNum;
+                nlastNum = currentNum;
+
+                
+            }
+            Console.WriteLine("The fibonacci number you asked to see is: " + nnextToLastNum);
+            Console.ReadLine();
+
+        }
+    }
+}
